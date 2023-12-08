ed1024f1f63a05be6b60cd5d4dc09874c822cbea
nadazkai21 <nadazaki019@gmail.com>
2023-12-08 23:19:44 +0200

    the getline function

--- _getline.c
+++ _getline.c
@@ -1,13 +1,17 @@
 #include "shell.h"
-#define MAX_BUFFER_SIZE 5000
+#define BUFFER_SIZE 5000
 /**
- * 
+ * _getline - gets all of the input line from stdin
+ * @lineptr: saves the input from stdin 
+ * @n: the number of charecters read form the stdin
+ * @stream: stream of input, if it was a file
+ * Return: the number of the charecters read
 */
 ssize_t _getline(char **lineptr, size_t *n, __attribute__ ((unused)) FILE *stream)
 {
     ssize_t read_bytes;
-    static char buffer[MAX_BUFFER_SIZE];
-
+    static char buffer[BUFFER_SIZE];
+		
     read_bytes = read(0, buffer, sizeof(buffer));
 
     *lineptr = buffer;
